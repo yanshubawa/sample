32cc678900e7cd69b9b71876c0fccbc5e16ec516
yanshu.bawa <yanshu.bawa@CHDSEZ309469L.ad.infosys.com>
2020-07-30 22:04:36 +0530

    commit 20

--- src/main/java/com/mkyong/web/controller/WelcomeController.java
+++ src/main/java/com/mkyong/web/controller/WelcomeController.java
@@ -23,7 +23,7 @@ public class WelcomeController {
     }
 
     public String getMessage() {
-        return "Hello World - Deployment Success 10";
+        return "Hello World - Deployment Success 20";
     }
 
 }
