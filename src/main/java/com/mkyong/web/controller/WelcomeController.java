e31eb035b0d21479aa72a89e7719247b3cf5bd13
yanshu.bawa <yanshu.bawa@CHDSEZ309469L.ad.infosys.com>
2020-07-30 14:21:54 +0530

    Updated Message

--- src/main/java/com/mkyong/web/controller/WelcomeController.java
+++ src/main/java/com/mkyong/web/controller/WelcomeController.java
@@ -23,7 +23,7 @@ public class WelcomeController {
     }
 
     public String getMessage() {
-        return "Hello World - Deployment";
+        return "Hello World - Deployment Success";
     }
 
 }
