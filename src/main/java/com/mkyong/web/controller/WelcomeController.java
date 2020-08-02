1998653ac1e3f9caeee2dc2854521b7761c9fdc3
yanshu.bawa <yanshu.bawa@10.110.113.23>
2020-08-03 01:05:27 +0530

    Commit

--- src/main/java/com/mkyong/web/controller/WelcomeController.java
+++ src/main/java/com/mkyong/web/controller/WelcomeController.java
@@ -23,7 +23,7 @@ public class WelcomeController {
     }
 
     public String getMessage() {
-        return "Hello World -AWS Code Deploy with Jfrog ";
+        return "Hello World -AWS Code Deploy with Jfrog Final ";
     }
 
 }
