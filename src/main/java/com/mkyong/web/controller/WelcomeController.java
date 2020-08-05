6f9618c56c61c9fb5a0007d57a00eae5cb56c20c
yanshu.bawa <yanshu.bawa@CHDSEZ309469L.ad.infosys.com>
2020-08-05 13:04:46 +0530

    wednesday commit

--- src/main/java/com/mkyong/web/controller/WelcomeController.java
+++ src/main/java/com/mkyong/web/controller/WelcomeController.java
@@ -23,7 +23,7 @@ public class WelcomeController {
     }
 
     public String getMessage() {
-        return "Hello World- AWS CODE DEPLOY 200";
+        return "Hello World- AWS CODE  deploy  wednesday result ";
     }
 
 }
